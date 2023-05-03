5393ae6ab374916651ca3aed4ad0deab23a6a685
Yasmin Marano <yaspachano@gmail.com>
2023-05-03 00:46:52 -0300

    ej 4 terminado

--- JavaScript/4.js
+++ JavaScript/4.js
@@ -1,11 +1,13 @@
+
+
 function palabraMasLarga(str) {
     if (typeof str !== "string") {
         return "ingresar una cadena de texto";
       }
     
-    const separadaPorEspacios = str.split(" ");
-    let palabraMasLarga = separadaPorEspacios[0];
-    for (let palabra of separadaPorEspacios) {
+    const palabras = str.split(" ");
+    let palabraMasLarga = palabras[0];
+    for (let palabra of palabras) {
         if (palabra.lenght >= palabraMasLarga.lenght) {
             palabraMasLarga = palabra;
             
@@ -16,3 +18,20 @@ function palabraMasLarga(str) {
 console.log(palabraMasLarga(45));
 // Ingresar una cadena de texto
 console.assert(palabraMasLarga('Erase una vez que se era') === 'Erase');
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+function primeraMayuscula(str) {
+    const palabras = str.split(" ");
+    for (let index = 0; index < palabras.length; index++) {
+     palabras[index] = palabras[index][0].toUpperCase() + palabras[index].substr(1);
+        
+    }
+     palabrasEnMayuscula = palabras.join(" ");
+
+   return palabrasEnMayuscula;
+}
+
+console.assert(primeraMayuscula("En un lugar de la Mancha") === "En Un Lugar De La Mancha");
