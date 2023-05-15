f250adf9daf20b4de5b6622681fb6879ab538e5e
Yasmin Marano <yaspachano@gmail.com>
2023-05-15 13:13:22 -0300

    update ejercicio 6

---
+++ JavaScript/6.js
@@ -0,0 +1,26 @@
+
+
+const carrito = {
+    productos: [
+        {
+            nombre: 'papel higienico',
+            unidades: 4,
+            precio: 5
+        },
+        {
+            nombre: 'queso',
+            unidades: 2,
+            precio: 6.5
+        }
+    ],
+    get precioTotal() {
+        let totalCarrito = 0;
+        for (let index = 0; index < this.productos.length; index++) {
+            totalCarrito = totalCarrito + this.productos[index].unidades * this.productos[index].precio;
+            
+        }
+        return totalCarrito;
+    }
+}; 
+
+console.log(carrito.precioTotal);
